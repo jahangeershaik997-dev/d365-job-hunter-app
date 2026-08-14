4058e31f93e4f94c57aa052efd056abbca59b5fb
jahangeershaik997-dev <jahangeershaik997@gmail.com>
2026-08-14 21:20:04 +0530

    Clean working Express app

--- server.js
+++ server.js
@@ -1,27 +1,9 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-app.get("/dashboard", (req, res) => {
-  res.render("dashboard");
-});
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => { res.render("index"); });
+if (require.main === module) { app.listen(process.env.PORT || 3000); }
+module.exports = app;
